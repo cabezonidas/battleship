6415f35333a3a8408c6c4b3c3886809ddad226f5
Sebastian Cabeza <sebastian.cabeza@noted.com>
2021-04-30 09:16:29 +1200

    Render random battleship grid

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { getRandomGrid } from "./ships";
+
+const grid = getRandomGrid();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <code style={{ display: "grid", gridTemplateColumns: "repeat(10, 30px)" }}>
+          {grid.map((row, i) => row.map((r, ii) => <div key={`${i}_${ii}`}>{r || "_"}</div>))}
+        </code>
       </header>
     </div>
   );
